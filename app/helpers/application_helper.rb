7a6482183043f945d5cd44e65937b7e39f2b0457
Bastien ROLANDO <bastien.rolando@gmail.com>
2017-09-03 20:46:44 +0200

    Fixing title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = "HelpMeChoose"
+    base_title = "MySuperUselessWebsite"
     if page_title.empty?
       base_title
     else
